1f0e6c0547e80fb3573db05836e3c38298875b86
Kyle S. Park <26356715+RuneLune@users.noreply.github.com>
2024-07-16 20:44:25 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240716/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cout << "Total days in Year\n365\nCircumference rate\n3.1415926535";
+    return 0;
+}
